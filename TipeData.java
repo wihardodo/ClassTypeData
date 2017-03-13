cb6f5c7775c7bd789f6ac519bc7b993ebabce3df
wihardodo <wihardodo@gmail.com>
2017-03-13 14:25:34 +0700

    Add files via upload

---
+++ TipeData.java
@@ -0,0 +1,58 @@
+public static void main(string) args){
+
+//type data objek
+String nama="Belajar Java Yuks";
+System.out.println(nama.length()};
+
+//type data byte
+byte maxByte=127;
+byte minByte=-128;
+byte defaultByte=0;
+System.out.println(defaultByte);
+
+
+//type data short
+short maxShort=32767;
+short minShort=-32768;
+short defaultShort=0;
+System.out.println(defaultShort);
+
+//type data int
+int maxInt=2147483647;
+int minInt=-2147483648;
+int defaultInt=0;
+System.out.println(defaultInt);
+
+//type data long
+long maxLong=9223372036854775807L;
+long maxLong=-9223372036854775808L;
+long maxLong=0L;
+System.out.println(defaultLong);
+
+//type data float
+float maxFloat=3.402834799999999999999999999f;
+float maxFloat=-3.4028347999999999999999999999f;
+float maxFloat=0.0f;
+System.out.println(maxfloat);
+System.out.println(minfloat);
+
+//type data double
+double randomDouble=1.797693134862315702d;
+double randomDouble1=-1.797693134862315602d;
+double defaultDouble=0.0;
+System.out.println(randomDouble);
+System.out.println(randomDouble1);
+
+//type data boolean
+boolean menikah=false;
+System.out.println(menikah);
+
+//type data char
+char A ='A';
+System.out.println(A);
+
+}
+
+
+
+
